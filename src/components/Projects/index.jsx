d5391cbcc4ef9149408f421d4a0db4150920232b
Gabriel <gabrielborgesschmidt@gmail.com>
2024-08-20 15:04:15 -0300

    removido nav da seção projetos

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -4,40 +4,40 @@ import { sumArray } from "../../helper";
 import { projects } from "../../data";
 import Card from "./Card";
 
-const tabs = [
-  {
-    name: "Todos",
-  },
-  {
-    name: "Web",
-  },
-  {
-    name: "Apps",
-  },
-];
+// const tabs = [
+//   {
+//     name: "Todos",
+//   },
+//   {
+//     name: "Web",
+//   },
+//   {
+//     name: "Apps",
+//   },
+// ];
 
 const Projects = () => {
   const [displayableProjects, setDisplayableProjects] = useState(projects);
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [offset, setOffset] = useState(0);
-  const [indicatorWidth, setIndicatorWidth] = useState(0);
-  const itemsEls = useRef(new Array());
+  // const [activeIndex, setActiveIndex] = useState(0);
+  // const [offset, setOffset] = useState(0);
+  // const [indicatorWidth, setIndicatorWidth] = useState(0);
+  // const itemsEls = useRef(new Array());
 
-  useEffect(() => {
-    const prevEl = itemsEls.current.filter((_, index) => index < activeIndex);
-    setOffset(sumArray(prevEl.map((item) => item.offsetWidth)));
-    setIndicatorWidth(itemsEls.current[activeIndex].offsetWidth);
-  }, [activeIndex]);
+  // useEffect(() => {
+  //   const prevEl = itemsEls.current.filter((_, index) => index < activeIndex);
+  //   setOffset(sumArray(prevEl.map((item) => item.offsetWidth)));
+  //   setIndicatorWidth(itemsEls.current[activeIndex].offsetWidth);
+  // }, [activeIndex]);
 
-  const setProjects = (category) => {
-    if (category === "All") {
-      return setDisplayableProjects(projects);
-    }
-    const pro = projects.filter(
-      (item) => item.category.toLowerCase() === category.toLowerCase()
-    );
-    setDisplayableProjects(pro);
-  };
+  // const setProjects = (category) => {
+  //   if (category === "All") {
+  //     return setDisplayableProjects(projects);
+  //   }
+  //   const pro = projects.filter(
+  //     (item) => item.category.toLowerCase() === category.toLowerCase()
+  //   );
+  //   setDisplayableProjects(pro);
+  // };
 
   return (
     <section id="projetos">
@@ -45,14 +45,13 @@ const Projects = () => {
         <div className="section__header center">
           <h1 className="primary__title">Projetos</h1>
         </div>
-        <nav>
+        {/* <nav>
           {tabs.map((tab, index) => (
             <button
               key={index}
               ref={(el) => (itemsEls.current[index] = el)}
               onClick={() => {
                 setActiveIndex(index);
-                setProjects(tab.name);
               }}
             >
               {tab.name}
@@ -65,7 +64,7 @@ const Projects = () => {
               width: `${indicatorWidth}px`,
             }}
           />
-        </nav>
+        </nav> */}
 
         <div className="card__container">
           {displayableProjects &&
